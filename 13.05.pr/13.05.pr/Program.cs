e9a33768c9a0de819eb9f5be107d441729b8d7ba
oromanyuk <65210841+oromanyuk@users.noreply.github.com>
2020-05-14 02:17:12 +0500

    Add files via upload

---
+++ 13.05.pr/13.05.pr/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _13._05.pr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "xeveer";
+            Console.WriteLine("1. Повтор: " + lib1.stpow(a, 3));
+            Console.WriteLine("2. Реверс: " + lib1.rev(a));
+            Console.WriteLine("3. Удаление: " + lib1.remov(a, "e"));
+            Console.WriteLine("4. Количество символов: " + lib1.leng(a));
+        }
+    }
+}
---
+++ 13.05.pr/13.05.pr/lib1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _13._05.pr
+{
+    public class lib1
+    {
+        static public string stpow(string a, int b)
+        {
+            string ag = "";
+            for(int i = 0; i < b; i++)
+            {
+                ag = ag + a;
+            }
+            a = ag;
+            return a;
+        }
+        static public string rev(string a)
+        {
+            string ag = "";
+            for(int i = a.Length - 1; i >= 0; i--)
+            {
+                ag = ag + a[i];
+            }
+            a = ag;
+            return a;
+        }
+        static public string remov(string a, string b)
+        {
+            a = a.Replace(b, "");
+            return a;
+        }
+        static public int leng(string a)
+        {
+
+            return a.Length;
+        }
+    }
+}
